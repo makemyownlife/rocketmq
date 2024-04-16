6f789b75aa87cf44efc43c7ceae6c05904b1e662
zhangyong <zhangyong7120180@163.com>
2024-04-16 10:25:54 +0800

    非自动提交 ，手工操作

--- client/src/test/java/org/apache/rocketmq/client/consumer/DefaultLitePullConsumerUnitTest.java
+++ client/src/test/java/org/apache/rocketmq/client/consumer/DefaultLitePullConsumerUnitTest.java
@@ -1,7 +1,10 @@
 package org.apache.rocketmq.client.consumer;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.message.MessageQueue;
 import org.junit.Test;
 
 import java.util.List;
@@ -31,6 +34,44 @@ public class DefaultLitePullConsumerUnitTest {
         } finally {
             litePullConsumer.shutdown();
         }
+    }
+
+    @Test
+    public void testNoAutoCommit() throws MQClientException {
+        boolean running = true;
+        // 定义消费者组 mygroup
+        DefaultLitePullConsumer litePullConsumer = new
+                DefaultLitePullConsumer("mygroup");
+        // 设置名字服务地址
+        litePullConsumer.setNamesrvAddr("127.0.0.1:9876");
+        // 从最新的进度偏移量
+        litePullConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+        // 订阅主题 TopicTest
+        litePullConsumer.subscribe("TopicTest", "*");
+        // 自动提交消费偏移量的选项设置为 false
+        litePullConsumer.setAutoCommit(false);
+        litePullConsumer.start();
+        try {
+            while (running) {
+                List<MessageExt> messageExts = litePullConsumer.poll();
+                if (CollectionUtils.isNotEmpty(messageExts)) {
+                    // 取出第一条消息数据
+                    MessageExt first = messageExts.get(0);
+                    // 打印消息内存
+                    System.out.println(new String(first.getBody()));
+                    MessageQueue messageQueue = new MessageQueue(first.getTopic(), first.getBrokerName(), first.getQueueId());
+                    // 回滚到第一条消息的点位
+                    litePullConsumer.seek(messageQueue, first.getQueueOffset());
+                }
+                Thread.sleep(5000);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            litePullConsumer.shutdown();
+        }
+
+
     }
 
 }
